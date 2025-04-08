1baee058a31112a169c827ccdb917bc0f776f208
Xarch <rakhaarkhana98@gmail.com>
2025-04-08 22:55:31 +0700

    fix vercel setup

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,33 @@
+const express = require("express");
 const serverless = require("serverless-http");
-const app = require("../src/index");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const addCancer = require("../src/controllers/addCancer");
+const getCancer = require("../src/controllers/getCancer");
+const getSingleCancer = require("../src/controllers/getSingleCancer");
+const deleteCancer = require("../src/controllers/deleteCancer");
+const editCancer = require("../src/controllers/editCancer");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API ANALISIS KANKER ONLINE NIH!");
+});
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Error", err));
+
+app.post("/api/predict", addCancer);
+app.get("/api/predict", getCancer);
+app.get("/api/predict/:id", getSingleCancer);
+app.delete("/api/predict/:id", deleteCancer);
+app.put("/api/predict/:id", editCancer);
+app.patch("/api/predict/:id", editCancer);
 
 module.exports = app;
 module.exports.handler = serverless(app);
--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const app = require("../api/index"); // manggil dari api/index.js
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-const addCancer = require("../src/controllers/addCancer");
-const getCancer = require("../src/controllers/getCancer");
-const getSingleCancer = require("../src/controllers/getSingleCancer");
-const deleteCancer = require("../src/controllers/deleteCancer");
-const editCancer = require("../src/controllers/editCancer");
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("API ANALISIS KANKER JALAN BRO!!");
+app.listen(PORT, () => {
+  console.log(`Server running locally at http://localhost:${PORT}`);
 });
-
-// Koneksi DB
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(() => console.log("MongoDB Failed to Connect"));
-
-// Route API
-app.post("/api/predict", addCancer);
-app.get("/api/predict", getCancer);
-app.get("/api/predict/:id", getSingleCancer);
-app.delete("/api/predict/:id", deleteCancer);
-app.put("/api/predict/:id", editCancer);
-app.patch("/api/predict/:id", editCancer);
-
-module.exports = app;
-module.exports.handler = serverless(app);
