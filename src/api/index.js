d75dc718cc3d9983ec39c2a280c119dd883b8a52
Xarch <rakhaarkhana98@gmail.com>
2025-04-08 22:38:35 +0700

    update terbaru api vercel

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,5 @@
-const express = require("express");
 const serverless = require("serverless-http");
+const app = require("../src/index"); // manggil express utama lu
 
-const app = express();
-
-// contoh route
-app.get("/", (req, res) => {
-  res.send("Hello from Vercel Express!");
-});
-
-// Export buat Vercel
 module.exports = app;
 module.exports.handler = serverless(app);
