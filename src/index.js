a72cd546644b3747726723db714e50675fda25e8
Xarch <rakhaarkhana98@gmail.com>
2025-04-08 23:00:12 +0700

    update vercel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-const app = require("../api/index"); // manggil dari api/index.js
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const addCancer = require("./controllers/addCancer");
+const getCancer = require("./controllers/getCancer");
+const getSingleCancer = require("./controllers/getSingleCancer");
+const deleteCancer = require("./controllers/deleteCancer");
+const editCancer = require("./controllers/editCancer");
 
-app.listen(PORT, () => {
-  console.log(`Server running locally at http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Berhasil terhubung ke app");
+});
+
+mongoose
+  .connect(process.env.MONGO_URL, {})
+  .then(() => {
+    console.log("berhasil terhubung ke MongoDB");
+  })
+  .catch((err) => {
+    console.log("gagal terhubung ke MongoDB");
+  });
+
+app.use(express.json());
+
+//models
+require("./models/cancerPrediction");
+
+//route
+app.post("/api/predict", addCancer);
+app.get("/api/predict", getCancer);
+app.get("/api/predict/:id", getSingleCancer);
+app.delete("/api/predict/:id", deleteCancer);
+app.put("/api/predict/:id", editCancer);
+app.patch("/api/predict/:id", editCancer);
+app.listen(3000, () => {
+  console.log(`app berjalan di http://localhost:3000`);
 });
