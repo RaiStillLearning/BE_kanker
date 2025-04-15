ffca6637c761dd664370c1a5ca0d48ede2ffd404
Xarch <rakhaarkhana98@gmail.com>
2025-04-15 12:05:01 +0700

    mnengubah port agar tidak bentrok dengan front-end

--- src/index.js
+++ src/index.js
@@ -180,6 +180,6 @@ app.patch("/api/predict/:id", async (req, res) => {
 });
 
 // Jalankan server
-app.listen(3000, () => {
-  console.log("✅ Server ready at http://localhost:3000");
+app.listen(5000, () => {
+  console.log("✅ Server ready at http://localhost:5000");
 });
